8fdd6afce77c7c0044fe5dedf6574ba329c35ca2
k2gaurav <kumargaurav22892@gmail.com>
2023-12-17 01:00:20 +0530

    path parameters

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/",description="This is our first route",deprecated=True)
+@app.get("/", description="This is our first route")
 async def root():
     return {"message": " hewllo word"}
 
@@ -14,5 +15,40 @@ async def post():
 
 
 @app.put("/")
-async def put ():
+async def put():
     return {"message": "hello from post route"}
+
+
+@app.get("/items")
+async def list_items():
+    return {"message": "list items route"}
+
+
+@app.get("/items/items_id")
+async def get_items(items_id: int):
+    return {"items_id": items_id}
+
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"message": "this is current user"}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {food_name: food_name, "message": "you like healthy food"}
+    if food_name == FoodEnum.fruits:
+        return {food_name: food_name, "message": "you like sweet product"}
+    return {food_name: food_name, "message": "you like dairy product"}
